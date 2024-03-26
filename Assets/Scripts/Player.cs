0739fbed059576c5ce72e4edd19d78bd2381b834
WooJiWon <woojiwon060317@gmail.com>
2024-03-26 20:25:09 +0900

    플레이어 스프라이트 및 애니메이션, 플레이어 이동 및 플립 기능 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        // 플레이어 이동 인풋
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // 플레이어 이동
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    private void LateUpdate()
+    {
+        // 플레이어 플립
+        if(inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x > 0;
+        }
+    }
+}

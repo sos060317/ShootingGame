2c5b6738aa036e61e2eb32ca0b1b813d1a671dbb
woojiwon <woojiwon060317@gmail.com>
2024-03-26 23:03:59 +0900

    무한맵 구현

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
-    }
+        // 플레이어 맵 경계선(Area)이 나갔는지 체크
+        if (!collision.CompareTag("Area"))
+            return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        float diffX = Mathf.Abs(playerPos.x - myPos.x);
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
+
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1;
+        float dirY = playerDir.y < 0 ? -1 : 1;
+
+        switch(transform.tag)
+        {
+            // 플레이어 이동 방향으로 위치 재설정
+            case "Ground":
+                if(diffX > diffY)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if(diffX < diffY)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+            case "Enemy":
+
+                break;
+        }
     }
 }

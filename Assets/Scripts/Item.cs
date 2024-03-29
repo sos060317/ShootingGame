c4b8f7d08ff27d05a778f08aca46530c6dc770e9
WooJiWon <woojiwon060317@gmail.com>
2024-03-29 20:30:39 +0900

    스킬 레벨 업 버트 구현

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ItemData data;
+    public int level;
+    public Weapon weapon;
+
+    Image icon;
+    Text textLevel;
+
+    private void Awake()
     {
-        
+        // 초기화
+        icon = GetComponentsInChildren<Image>()[2];
+        icon.sprite = data.itemIcon;
+
+        Text[] texts = GetComponentsInChildren<Text>();
+        textLevel = texts[0];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        // 레벨 텍스트 초기화
+        textLevel.text = "Lv." + (level + 1);
+    }
+
+    public void OnClick()
+    {
+        switch(data.itemType)
+        {
+            case ItemData.ItemType.Melee:
+            case ItemData.ItemType.Range:
+
+                break;
+            case ItemData.ItemType.Glove:
+                break;
+            case ItemData.ItemType.Shoe:
+                break;
+            case ItemData.ItemType.Heal:
+                break;
+        }
+
+        level++;
+
+        if (level == data.damages.Length)
+            GetComponent<Button>().interactable = false;
     }
 }

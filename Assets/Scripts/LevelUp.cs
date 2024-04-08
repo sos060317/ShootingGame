dd9ae99e8c2aade2b1bc8305f867c931d755cc59
WooJiWon <woojiwon060317@gmail.com>
2024-04-08 20:51:31 +0900

    레벨 업 시 겹치지 않게 카드 뽑기 구현 중...

--- Assets/Scripts/LevelUp.cs
+++ Assets/Scripts/LevelUp.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = System.Random;
 
 public class LevelUp : MonoBehaviour
 {
@@ -28,5 +30,26 @@ public class LevelUp : MonoBehaviour
     public void Select(int index)
     {
         items[index].OnClick();
+    }
+
+    void Next()
+    {
+        foreach(Item item in items)
+        {
+            item.gameObject.SetActive(false);
+        }
+
+        // 배열 생성
+        int[] ran = new int[3];
+
+        // 중복을 제거하고 요소를 무작위로 선택
+        Random random = new Random();
+        var uniqueRandomNumbers = ran.OrderBy(x => random.Next()).Take(3);
+
+        Debug.Log("Unique Random Numbers:");
+        foreach (var number in uniqueRandomNumbers)
+        {
+            Debug.Log(number);
+        }
     }
 }
